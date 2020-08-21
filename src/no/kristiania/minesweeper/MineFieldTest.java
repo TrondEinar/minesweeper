17fafc834ca4b772ea9ced47a5c2a9298a9c6ff1
Johbra96 <61549336+Johbra96@users.noreply.github.com>
2020-08-21 14:54:52 +0200

    Correct rows green, correct columns red

--- src/no/kristiania/minesweeper/MineFieldTest.java
+++ src/no/kristiania/minesweeper/MineFieldTest.java
@@ -12,14 +12,26 @@ public class MineFieldTest {
     }
 
     @Test
-    void isShowsCorrectNumberOfRows() {
+    void itShowsCorrectNumberOfRows() {
         assertArrayEquals(new String[] {"000" , "000" , "000"} ,
                 displayMinefield(new String[] {"..." , "..." , "..."}));
     }
+
+    @Test
+    void itShowsCorrectNumberOfColumns() {
+        assertArrayEquals(
+                new String[] { "0000" },
+                displayMinefield(new String[] {"...."})
+        );
+    }
+
     private String[] displayMinefield(String[] input) {
-        return new String[] {
-                "000" , "000"
-        };
+        String[] result = new String [input.length];
+        for (int i = 0; i < input.length; i++) {
+            result[i] = "000";
+            
+        }
+        return result;
     }
 
 }
